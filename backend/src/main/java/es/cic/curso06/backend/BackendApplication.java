05222fabe023c4d2d97683b7a8eb3839a81c4837
Diego <mi@email.com>
2024-09-09 08:39:06 +0200

    Backend

---
+++ backend/src/main/java/es/cic/curso06/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package es.cic.curso06.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
