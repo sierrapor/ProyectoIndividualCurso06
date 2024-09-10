f84fde5afd0cdc95de866a117bfb9fef3a885832
Diego <mi@email.com>
2024-09-10 08:17:40 +0200

    limpieza iconos helloworld, creacion vistas y enrutado

---
+++ Frontend/src/views/Astros.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div>
+    <h1>Astros</h1>
+  </div>
+</template>
---
+++ Frontend/src/views/AstrosForm.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div>
+    <h1>AstrosForm</h1>
+  </div>
+</template>
